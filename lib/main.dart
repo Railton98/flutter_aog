2fbb223e2f356bdf281eb7971e14b2625d2a0e2a
Railton98 <railtonleal98@gmail.com>
2019-11-18 16:32:04 -0300

    calculate first part

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,5 @@
-import 'package:aog/widgets/input.widget.dart';
-import 'package:aog/widgets/loading-buttom.widget.dart';
-import 'package:aog/widgets/logo.widget.dart';
-import 'package:aog/widgets/submit-form.dart';
-import 'package:aog/widgets/success.widget.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_masked_text/flutter_masked_text.dart';
+import 'pages/home.page.dart';
 
 void main() => runApp(MyApp());
 
@@ -16,29 +11,6 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.deepPurple),
       home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  var _gasCtrl = new MoneyMaskedTextController();
-  var _alcCtrl = new MoneyMaskedTextController();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Theme.of(context).primaryColor,
-      body: ListView(
-        children: <Widget>[
-          Logo(),
-          SubmitForm(
-            alcCtrl: _alcCtrl,
-            gasCtrl: _gasCtrl,
-            submitFunc: () {},
-            busy: false,
-          ),
-        ],
-      ),
     );
   }
 }
