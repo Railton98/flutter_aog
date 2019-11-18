479daf8e9b59353d3bb9312ac6d23f3b0197bba9
Railton98 <railtonleal98@gmail.com>
2019-11-18 13:13:02 -0300

    loading button

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:aog/widgets/input.widget.dart';
+import 'package:aog/widgets/loading-buttom.widget.dart';
 import 'package:aog/widgets/logo.widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_masked_text/flutter_masked_text.dart';
@@ -51,25 +52,11 @@ class HomePage extends StatelessWidget {
                 SizedBox(
                   height: 20,
                 ),
-                Container(
-                  margin: EdgeInsets.all(30),
-                  height: 60,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    color: Colors.white.withOpacity(0.8),
-                    borderRadius: BorderRadius.circular(60),
-                  ),
-                  child: FlatButton(
-                    child: Text(
-                      "CALCULAR NOVAMENTE",
-                      style: TextStyle(
-                        color: Theme.of(context).primaryColor,
-                        fontSize: 25,
-                        fontFamily: "Big Shoulders Display",
-                      ),
-                    ),
-                    onPressed: () {},
-                  ),
+                LoadingButtom(
+                  busy: false,
+                  func: () {},
+                  text: "CALCULAR NOVAMENTE",
+                  invert: true,
                 ),
               ],
             ),
@@ -82,24 +69,11 @@ class HomePage extends StatelessWidget {
             ctrl: _alcCtrl,
             label: "Álcool",
           ),
-          Container(
-            margin: EdgeInsets.all(30),
-            height: 60,
-            decoration: BoxDecoration(
-              color: Colors.white.withOpacity(0.8),
-              borderRadius: BorderRadius.circular(60),
-            ),
-            child: FlatButton(
-              child: Text(
-                "CALCULAR",
-                style: TextStyle(
-                  color: Theme.of(context).primaryColor,
-                  fontSize: 25,
-                  fontFamily: "Big Shoulders Display",
-                ),
-              ),
-              onPressed: () {},
-            ),
+          LoadingButtom(
+            busy: true,
+            func: () {},
+            text: "CALCULAR",
+            invert: false,
           ),
         ],
       ),
