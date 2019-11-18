25654f227c4f56add908f4c104440fb9cf90e957
Railton98 <railtonleal98@gmail.com>
2019-11-18 16:48:45 -0300

    animated background

--- lib/pages/home.page.dart
+++ lib/pages/home.page.dart
@@ -10,6 +10,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  Color _color = Colors.deepPurple;
   var _gasCtrl = new MoneyMaskedTextController();
   var _alcCtrl = new MoneyMaskedTextController();
   var _busy = false;
@@ -20,21 +21,27 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).primaryColor,
-      body: ListView(
-        children: <Widget>[
-          Logo(),
-          _completed
-              ? Success(
-                  result: _resultText,
-                  reset: reset,
-                )
-              : SubmitForm(
-                  alcCtrl: _alcCtrl,
-                  gasCtrl: _gasCtrl,
-                  submitFunc: calculate,
-                  busy: _busy,
-                ),
-        ],
+      body: AnimatedContainer(
+        duration: Duration(
+          milliseconds: 1200,
+        ),
+        color: _color,
+        child: ListView(
+          children: <Widget>[
+            Logo(),
+            _completed
+                ? Success(
+                    result: _resultText,
+                    reset: reset,
+                  )
+                : SubmitForm(
+                    alcCtrl: _alcCtrl,
+                    gasCtrl: _gasCtrl,
+                    submitFunc: calculate,
+                    busy: _busy,
+                  ),
+          ],
+        ),
       ),
     );
   }
@@ -47,6 +54,7 @@ class _HomePageState extends State<HomePage> {
     double res = alc / gas;
 
     setState(() {
+      _color = Colors.deepPurpleAccent;
       _completed = false;
       _busy = true;
     });
@@ -71,6 +79,7 @@ class _HomePageState extends State<HomePage> {
       _gasCtrl = new MoneyMaskedTextController();
       _completed = false;
       _busy = false;
+      _color = Colors.deepPurple;
     });
   }
 }
